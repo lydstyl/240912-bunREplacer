9afa33f83f5b2258a1ae08fced508fa86c5c2399
Gabriel Brun <lydstyl@gmail.com>
2024-09-16 16:14:17 +0200

    Add new regex and steps.

--- index.ts
+++ index.ts
@@ -1,101 +1,75 @@
 let text = `
-Base AGROBIOTHERS_cypress – connexion avec l’utilisateur Anne
+FILET ZINGER 80 gr 8 x 20pcs
 
-Entrer dans la base – Appliquer
+ KF610416-HOTWINGS PREMARINE S 35GRS 6 X 68 PCS
 
-Menu – Approvisionnement
+ FILET OR 80gr
 
-Ecran - Commandes à valider
+ Strips pré marinés
 
-Choisir la configuration « TestCommande » onglet Tous
-
-Cocher les deux premières lignes
-
-Faire clic sur le bouton à PASSER COMMANDE
-
-Faire clic sur Valider les commandes
-
-Vérifier qu’on a bien deux fichiers (un pour chaque commande) dans le dossier (D:\Azap\Export\Agro) :
-
-PropCde_F04929_ CUISERY_(datedujour).csv
-
-PropCde_F04929_ CUISERY_(datedujour).csv
-
-Ouvrir le fichier PropCde_F04929_ CUISERY_(datedujour).csv et vérifier les dates :
-
-DateCommande : 08/11/2023
-
-DateMAD : 29/11/2023
-
-DateLivraison : 29/11/2023
-
-11.   Vérifier qu’on a bien les produits :
-
-a.      126922
-
-b.     224652
-
-c.      107052
-
-d.     914492
-
-e.      923512
-
-f.       923932
-
-g.      924682
-
-h.     924912
-
-Ouvrir le fichier PropCde_F04929_ CUISERY_(datedujour).csv et vérifier les dates :
-
-DateCommande : 22/11/2023
-
-DateMAD : 13/12/2023
-
-DateLivraison : 13/12/2023
-
-13.   Vérifier qu’on a bien les produits :
-
-a.      110642
-
-b.     217632
-
-c.      202492
-
-d.     752552
-
-e.      915172
-
-f.       923452
-
-g.      924842
+KF611038-MINI TENDERS 9x80pcs
 `
 
-const lineBreak = /(\r\n|\n|\r)/g
+const re = {
+  lineBreak: /(\r\n|\n|\r)/g,
+  lineBreak2: /(\r\n|\n|\r){2}/g
+}
 
-const azDiscriptionSteps = [
-  {
-    regex: /(.*)$/gm,
-    replacer: '// $1'
-  },
-  {
-    regex: /\/{2}\s$/gm,
-    replacer: '\n'
-  },
-  {
-    regex: /(\r\n|\n|\r){2}/g,
-    replacer: '\n'
-  },
-  {
-    regex: /(\r\n|\n|\r){2}/g,
-    replacer: '\n'
-  }
-]
+const steps = {
+  azDiscriptionSteps: [
+    {
+      regex: /(.*)$/gm,
+      replacer: '// $1'
+    },
+    {
+      regex: /\/{2}\s$/gm,
+      replacer: '\n'
+    },
+    {
+      regex: /(\r\n|\n|\r){2}/g,
+      replacer: '\n'
+    },
+    {
+      regex: /(\r\n|\n|\r){2}/g,
+      replacer: '\n'
+    }
+  ],
+  createArray: [
+    {
+      regex: re.lineBreak2, // replace 2 line breaks with one
+      replacer: '\n'
+    },
+    {
+      regex: /^\s/gm, // remove first space
+      replacer: ''
+    },
+    {
+      // regex: /^((\w|-)+)/gm,
+      regex: /^(.+)/gm, // wrap work with quotes
+      replacer: '"$1"'
+    },
+    {
+      regex: re.lineBreak, // replace line breaks with comma
+      replacer: ','
+    },
+    {
+      regex: /^,/g, // replace first comma with [
+      replacer: '['
+    },
+    {
+      regex: /,$/g, // replace last comma with ]
+      replacer: ']'
+    },
+    {
+      regex: /^"/g, // replace first quote with [ only if first character is a quote
+      replacer: '["'
+    }
+  ]
+}
 
-azDiscriptionSteps.forEach((step) => {
+steps.createArray.forEach((step) => {
   text = text.replace(step.regex, step.replacer)
-  console.log(text)
 })
+console.log(text)
 
 // bun index.ts
