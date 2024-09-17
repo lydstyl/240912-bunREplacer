a6cafc779c134cca80ac031c91d3cde9619ee633
Gabriel Brun <lydstyl@gmail.com>
2024-09-17 09:12:29 +0200

    Add a new steps to create array of pictures.

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 let text = `
-FILET ZINGER 80 gr 8 x 20pcs
-
- KF610416-HOTWINGS PREMARINE S 35GRS 6 X 68 PCS
-
- FILET OR 80gr
-
- Strips pré marinés
-
-KF611038-MINI TENDERS 9x80pcs
+https://2023-923ce00f-e7c6-499a-96bf-ff03fbab8e82-gi.s3.toutemonannee.com/n1/42d25347-40b6-4510-8f06-b1037b39aaf8/hd/f06106d1-3942-46bc-a9b0-3f0723a95232.jpg?lastmod=1721985322
+chunk-FYWFTXCL.js:5 https://2023-923ce00f-e7c6-499a-96bf-ff03fbab8e82-gi.s3.toutemonannee.com/n1/42d25347-40b6-4510-8f06-b1037b39aaf8/hd/2e7c34b7-a274-41a0-a5bf-e97a760c93db.jpg?lastmod=1709890602
+chunk-FYWFTXCL.js:5 https://2023-923ce00f-e7c6-499a-96bf-ff03fbab8e82-gi.s3.toutemonannee.com/n1/42d25347-40b6-4510-8f06-b1037b39aaf8/hd/5338cb64-4010-43cc-9a7b-e96275d686d0.jpg?lastmod=1715099651
+chunk-FYWFTXCL.js:5 https://2023-923ce00f-e7c6-499a-96bf-ff03fbab8e82-gi.s3.toutemonannee.com/n1/42d25347-40b6-4510-8f06-b1037b39aaf8/hd/69ede387-2d0c-4515-9911-ae7bde2cee7e.jpg?lastmod=1715099654
+undefined
+document.querySelectorAll('img.lg-image').forEach(img => console.log(img.src))
 `
 
 const re = {
@@ -64,10 +61,44 @@ const steps = {
       regex: /^"/g, // replace first quote with [ only if first character is a quote
       replacer: '["'
     }
+  ],
+  createArrayTouteMonAnnee: [
+    {
+      regex: /^\s/gm, // remove first space
+      replacer: ''
+    },
+    {
+      regex: /^.*(http.+)/gm, // wrap work with quotes
+      replacer: '"$1"'
+    },
+    {
+      regex: /^[^"http].*/gm, // remove not starting with "http"
+      replacer: ''
+    },
+    {
+      regex: re.lineBreak2, // replace 2 line breaks with one
+      replacer: '\n'
+    },
+    {
+      regex: re.lineBreak2, // replace 2 line breaks with one
+      replacer: '\n'
+    },
+    {
+      regex: re.lineBreak, // replace line breaks with comma
+      replacer: ','
+    },
+    {
+      regex: /,$/g, // replace last comma with ]
+      replacer: ']'
+    },
+    {
+      regex: /^"/g, // replace first quote with [ only if first character is a quote
+      replacer: '["'
+    }
   ]
 }
 
-steps.createArray.forEach((step) => {
+steps.createArrayTouteMonAnnee.forEach((step) => {
   text = text.replace(step.regex, step.replacer)
 })
 console.log(text)
